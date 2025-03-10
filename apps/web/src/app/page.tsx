3427421b5122ba9f39d3fdbef0acc1f18d453379
Pallavi <pallavilpmt1995@gmail.com>
2025-03-10 21:43:01 +0530

    initial commit

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from 'next';
+import { Button } from '@ui/components/button';
+
+
+
+export const metadata: Metadata = {
+  title: 'Web - Turborepo Example',
+};
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <main className="mx-auto w-auto px-4 pb-8 pt-16 sm:pt-24 lg:px-8">
+       
+      </main>
+    </div>
+  );
+}
