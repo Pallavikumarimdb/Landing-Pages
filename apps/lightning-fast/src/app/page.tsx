6cdb4569f839d244cb307afdec569afb179f76b8
Pallavi <pallavilpmt1995@gmail.com>
2025-03-18 23:40:40 +0530

    lightning project

---
+++ apps/lightning-fast/src/app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import HeroSection from "../components/pages/HeroSection";
+
+
+
+export default function Home() {
+
+
+  return (
+    <div className='bg-[#E2F1E7]'>
+      <HeroSection/>
+  </div>
+  );
+}
